9049543fc9dc3a4c49596a4fad6558a1745a476e
Piyanshu Saini <priyanshusaininew@gmail.com>
2024-04-04 01:05:16 +0530

    Implement reveal animation

--- src/components/AR.tsx
+++ src/components/AR.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Spline from "@splinetool/react-spline";
 import React from "react";
+import { motion } from "framer-motion";
 
 const AR = () => {
   return (
@@ -12,15 +13,26 @@ const AR = () => {
         />
       </div>
       <div className="p-4 flex justify-end flex-col">
-        <h2 className="md:text-4xl text-3xl font-bold text-white font-futura whitespace-nowrap">
+        <motion.h2 className="md:text-4xl text-3xl font-bold text-white font-futura whitespace-nowrap"
+          initial={{ y: -50, opacity: 0 }}
+          exit={{ y: -50, opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          whileInView={{ y: 0, opacity: 1 }}
+        >
           Augmented Reality
-        </h2>
-        <p className="mt-2  text-white  font-work-sans text-xl md:max-w-96">
+        </motion.h2>
+        <motion.p
+          className="mt-2  text-white  font-work-sans text-xl md:max-w-96"
+          initial={{ x: 150, opacity: 0 }}
+          exit={{ x: 150, opacity: 0 }}
+          transition={{ duration: 0.4,delay:0.3 }}
+          whileInView={{ x: 0, opacity: 1 }}
+        >
           Augmented Reality (AR) is like magic! It mixes the real world with
           awesome digital elements, enhancing your view with cool info, videos,
           or games right in front of you. It's fun, futuristic, and super
           engaging!
-        </p>
+        </motion.p>
       </div>
     </div>
   );
--- src/components/VR.tsx
+++ src/components/VR.tsx
@@ -1,20 +1,33 @@
 "use client";
 import Spline from "@splinetool/react-spline";
 import React from "react";
+import { motion } from "framer-motion";
 
 const VR = () => {
   return (
     <div className="flex mx-auto w-[90%] flex-col md:flex-row items-center justify-center md:justify-between mt-6">
       <div className="md:w-1/2 p-4">
-        <h2 className="font-bold text-white text-3xl font-futura sm:text-4xl">
+        <motion.h2
+          className="font-bold text-white text-3xl font-futura sm:text-4xl"
+          initial={{ y: -50, opacity: 0 }}
+          exit={{ y: -50, opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          whileInView={{ y: 0, opacity: 1 }}
+        >
           Virtual Reality
-        </h2>
-        <p className="mt-2 text-white font-work-sans text-xl md:max-w-96">
+        </motion.h2>
+        <motion.p
+          className="mt-2 text-white font-work-sans text-xl md:max-w-96"
+          initial={{ x: -150, opacity: 0 }}
+          exit={{ x: -150, opacity: 0 }}
+          transition={{ duration: 0.4, delay: 0.3 }}
+          whileInView={{ x: 0, opacity: 1 }}
+        >
           Virtual Reality (VR) is pure immersion! It whisks you away from
           reality into mind-blowing digital realms. With a headset, you're
           transported to thrilling adventures, lifelike simulations, and
           breathtaking experiences that feel unbelievably real!
-        </p>
+        </motion.p>
       </div>
       <div className="md:w-1/2 flex md:justify-end mx-auto">
         <Spline
