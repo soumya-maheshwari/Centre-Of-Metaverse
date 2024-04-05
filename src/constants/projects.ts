5326874cb48101b93f28eba3b38fb2153c913eda
Piyanshu Saini <priyanshusaininew@gmail.com>
2024-04-05 17:22:22 +0530

    Change project images

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -13,27 +13,22 @@ export const projects: Project[] = [
   {
     name: "Vr Rescue X",
     description: "A VR game that helps people with phobias.",
-    image: "/vr-rescue-x.png",
+    image: "/project2.png",
   },
   {
     name: "Vr Rescue X",
     description: "A VR game that helps people with phobias.",
-    image: "/vr-rescue-x.png",
+    image: "/project3.png",
   },
   {
     name: "Vr Rescue X",
     description: "A VR game that helps people with phobias.",
-    image: "/vr-rescue-x.png",
+    image: "/project4.png",
   },
   {
     name: "Vr Rescue X",
     description: "A VR game that helps people with phobias.",
-    image: "/vr-rescue-x.png",
-  },
-  {
-    name: "Vr Rescue X",
-    description: "A VR game that helps people with phobias.",
-    image: "/vr-rescue-x.png",
+    image: "/project5.png",
   },
 
 ];
