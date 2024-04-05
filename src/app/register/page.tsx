11f01aab24056419bc74d86f137d986425f437b4
Piyanshu Saini <priyanshusaininew@gmail.com>
2024-04-05 19:23:13 +0530

    Performance improvement and add linkedin to socialmedia

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,17 +1,29 @@
+"use client";
 import Navbar from "@/components/Navbar";
+import Script from "next/script";
 import React from "react";
 
 const Page = () => {
   return (
     <main className="w-screen h-screen bg-white overflow-x-hidden">
-        <Navbar isDark />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-  <div class="visme_d" data-title="MetaRegistration" data-url="90xe846k-metaregistration" data-domain="forms" data-full-page="false" data-min-height="500px" data-form-id="53222"></div><script src="https://static-bundles.visme.co/forms/vismeforms-embed.js"></script>
-  `,
+      <Navbar isDark />
+      <div>
+        <div
+          className="visme_d"
+          data-title="MetaRegistration"
+          data-url="90xe846k-metaregistration"
+          data-domain="forms"
+          data-full-page="false"
+          data-min-height="500px"
+          data-form-id="53222"
+        ></div>
+      </div>
+      <Script
+        src="https://static-bundles.visme.co/forms/vismeforms-embed.js"
+        strategy="beforeInteractive"
+        onReady={() => {
+          console.log("Script loaded");
         }}
-        className="w-full h-full mt-[4rem]"
       />
     </main>
   );
