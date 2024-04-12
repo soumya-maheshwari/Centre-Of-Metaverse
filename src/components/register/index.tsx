42ac8afd08103d10ef85c5e14d94a1db8adbc01e
Piyanshu Saini <priyanshusaininew@gmail.com>
2024-04-12 14:18:47 +0530

    Form body

--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -6,7 +6,8 @@ type FormValues = {
   email: string;
   studentId: string;
   phoneNumber: string;
-  year: string;
+  gender:"Male"|"Female";
+  year: "1st Year"|"2nd Year";
   branch:
     | "CSE"
     | "CSE-DS"
@@ -23,6 +24,7 @@ type FormValues = {
   isHosteler: boolean;
   section: string;
 };
+
 export function RegistrationForm() {
   const {
     register,
@@ -30,72 +32,99 @@ export function RegistrationForm() {
     watch,
     formState: { errors },
   } = useForm<FormValues>();
-  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);}
+
 
   return (
     <section className="">
-      <div></div>
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="firstName">First Name</label>
-                <input {...register("firstName", { required: true })} />
-                {errors.firstName && <span>This field is required</span>}
+    <div></div>
+    <div className="bg-white">
+        <h1 className="font-semibold font-work-sans text-4xl">
+            Enter Your Details
+        </h1>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="font-work-sans text-[2A2A2A] font-semibold text-xl"
+        >
+          <div className="flex gap-3">
+            <div className="flex flex-col">
+              <label htmlFor="firstName" className="text-[2A2A2A]">
+                First Name
+              </label>
+              <input
+                placeholder="Enter your First Name"
+                {...register("firstName", { required: true })}
+                className="form-field"
+              />
+              {errors.firstName && <span>This field is required</span>}
             </div>
-            <div>
-                <label htmlFor="lastName">Last Name</label>
-                <input {...register("lastName")} />
+            <div className="flex flex-col">
+              <label htmlFor="lastName" className="text-[2A2A2A]">
+                Last Name
+              </label>
+              <input
+                placeholder=""
+                {...register("lastName", { required: true })}
+                className="form-field"
+              />
+              {errors.lastName && <span>This field is required</span>}
             </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
-                {errors.email && <span>Please enter a valid email address</span>}
-            </div>
-            <div>
-                <label htmlFor="studentId">Student ID</label>
-                <input {...register("studentId", { required: true })} />
-                {errors.studentId && <span>This field is required</span>}
-            </div>
-            <div>
-                <label htmlFor="phoneNumber">Phone Number</label>
-                <input {...register("phoneNumber", { required: true })} />
-                {errors.phoneNumber && <span>This field is required</span>}
-            </div>
-            <div>
-                <label htmlFor="year">Year</label>
-                <input {...register("year", { required: true })} />
-                {errors.year && <span>This field is required</span>}
-            </div>
-            <div>
-                <label htmlFor="branch">Branch</label>
-                <select {...register("branch", { required: true })}>
-                    <option value="CSE">CSE</option>
-                    <option value="CSE-DS">CSE-DS</option>
-                    <option value="CSE-AIML">CSE-AIML</option>
-                    <option value="AIML">AIML</option>
-                    <option value="CSIT">CSIT</option>
-                    <option value="IT">IT</option>
-                    <option value="CSE-Hindi">CSE-Hindi</option>
-                    <option value="CS">CS</option>
-                    <option value="ECE">ECE</option>
-                    <option value="ME">ME</option>
-                    <option value="CE">CE</option>
-                    <option value="EN">EN</option>
-                </select>
-                {errors.branch && <span>This field is required</span>}
-            </div>
-            <div>
-                <label htmlFor="isHosteler">Is Hosteler?</label>
-                <input type="checkbox" {...register("isHosteler")} />
-            </div>
-            <div>
-                <label htmlFor="section">Section</label>
-                <input {...register("section", { required: true })} />
-                {errors.section && <span>This field is required</span>}
-            </div>
-            <button type="submit">Submit</button>
+          </div>
+
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+            />
+            {errors.email && <span>Please enter a valid email address</span>}
+          </div>
+          <div>
+            <label htmlFor="studentId">Student ID</label>
+            <input {...register("studentId", { required: true })} />
+            {errors.studentId && <span>This field is required</span>}
+          </div>
+          <div>
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <input {...register("phoneNumber", { required: true })} />
+            {errors.phoneNumber && <span>This field is required</span>}
+          </div>
+          <div>
+            <label htmlFor="year">Year</label>
+            <input {...register("year", { required: true })} />
+            {errors.year && <span>This field is required</span>}
+          </div>
+          <div>
+            <label htmlFor="branch">Branch</label>
+            <select {...register("branch", { required: true })}>
+              <option value="CSE">CSE</option>
+              <option value="CSE-DS">CSE-DS</option>
+              <option value="CSE-AIML">CSE-AIML</option>
+              <option value="AIML">AIML</option>
+              <option value="CSIT">CSIT</option>
+              <option value="IT">IT</option>
+              <option value="CSE-Hindi">CSE-Hindi</option>
+              <option value="CS">CS</option>
+              <option value="ECE">ECE</option>
+              <option value="ME">ME</option>
+              <option value="CE">CE</option>
+              <option value="EN">EN</option>
+            </select>
+            {errors.branch && <span>This field is required</span>}
+          </div>
+          <div>
+            <label htmlFor="isHosteler">Is Hosteler?</label>
+            <input type="checkbox" {...register("isHosteler")} />
+          </div>
+          <div>
+            <label htmlFor="section">Section</label>
+            <input {...register("section", { required: true })} />
+            {errors.section && <span>This field is required</span>}
+          </div>
+          <button type="submit">Submit</button>
         </form>
-      </div>
-    </section>
+    </div>
+</section>
+
   );
 }
