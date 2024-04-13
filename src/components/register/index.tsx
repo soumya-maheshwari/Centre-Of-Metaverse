542305c633a19c28106cd39b05cd3fae6c9500b6
Piyanshu Saini <priyanshusaininew@gmail.com>
2024-04-14 01:52:41 +0530

    Create form validation

--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -1,215 +1,232 @@
 "use client";
-import { useForm, SubmitHandler } from "react-hook-form";
-type FormValues = {
-  Name: string;
-  email: string;
-  studentId: string;
-  phoneNumber: string;
-  gender: "Male" | "Female" | "Other";
-  branch:
-    | "AIML"
-    | "CE"
-    | "CSE"
-    | "CSE-AIML"
-    | "CSE-DS"
-    | "CSE-Hindi"
-    | "CS"
-    | "CSIT"
-    | "ECE"
-    | "EN"
-    | "IT"
-    | "ME";
+import { useForm, SubmitHandler, useFormState } from "react-hook-form";
+import { TracingBeam } from "@/components";
+import { useMemo } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import { FormValues } from "@/type";
 
-  Residency: "Hosteller" | "Day scholar";
-  section:
-    | "S1"
-    | "S2"
-    | "S3"
-    | "S4"
-    | "S5"
-    | "S6"
-    | "S7"
-    | "S8"
-    | "S9"
-    | "S10"
-    | "S11"
-    | "S12"
-    | "S13"
-    | "S14"
-    | "S15"
-    | "S16"
-    | "S17"
-    | "S18"
-    | "S19"
-    | "S20";
-};
 
 export function RegistrationForm() {
   const {
     register,
     handleSubmit,
     watch,
+    getValues,
     formState: { errors },
   } = useForm<FormValues>();
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
   };
 
+  const numberOfFieldsFilled = Object.values(getValues());
+
+  const sections = useMemo(
+    () => [
+      "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10",
+      "S11", "S12", "S13", "S14", "S15", "S16", "S17", "S18", "S19", "S20"
+    ],
+    []
+  );
+
+  const branches = useMemo(
+    () => [
+      "AIML", "CE", "CSE", "CSE-AIML", "CSE-DS", "CSE-Hindi",
+      "CS", "CSIT", "ECE", "EN", "IT", "ME"
+    ],
+    []
+  );
+
+
   return (
-    <section className="min-h-screen flex">
-      <div className="bg-black w-1/3 h-full"></div>
-      <div className="bg-white md:px-8 py-4 mx-auto leading-10 w-3/4">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="font-work-sans font-semibold text-xl w-[80%] mx-auto"
-        >
-          <h1 className="vision-head text-center mt-4 font-bungeeInline">
-            VISION
-          </h1>
-          <div className="mt-8">
-            <label htmlFor="name" className="block mb-1 text-sm col-12">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              placeholder="Name"
-              className="md:h-10 w-full rounded-md border text-black  border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-            />
-          </div>
-
-          <div className="mt-4">
-            <label htmlFor="Email" className="block mb-1 text-sm">
-              College Email
-            </label>
-            <input
-              type="text"
-              id="email"
-              placeholder="@akgec.ac.in"
-              className="md:h-10 w-full rounded-md border text-black  border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-            />
-          </div>
-
-          <div className="mt-4">
-            <label htmlFor="PhoneNumber" className="block mb-1 text-sm">
-              Phone Number
-            </label>
-            <input
-              type="text"
-              id="PhoneNumber"
-              placeholder="Phone Number"
-              className="md:h-10 w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-            />
-          </div>
-
-          <div className="mt-4">
-            <label htmlFor="studentNumber" className="block mb-1 text-sm">
-              Student Number
-            </label>
-            <input
-              type="text"
-              id="studentNumber"
-              placeholder="student Number"
-              className="h-10 w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-            />
-          </div>
-
-          <div className="flex space-x-4 w-full">
-            <div className="mt-4 w-1/2">
-              <label htmlFor="Section" className="block mb-1 text-sm">
-                Section
-              </label>
-              <select
-                id="Section"
-                className="md:h-10 block w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-              >
-                <option className="w-full">Select Section </option>
-
-                <option value="S1">S1</option>
-                <option value="S2">S2</option>
-                <option value="S3">S3</option>
-                <option value="S4">S4</option>
-                <option value="S5">S5</option>
-                <option value="S6">S6</option>
-                <option value="S7">S7</option>
-                <option value="S8">S8</option>
-                <option value="S9">S9</option>
-                <option value="S10">S10</option>
-                <option value="S11">S11</option>
-                <option value="S12">S12</option>
-                <option value="S13">S13</option>
-                <option value="S14">S14</option>
-                <option value="S15">S15</option>
-                <option value="S16">S16</option>
-                <option value="S17">S17</option>
-                <option value="S18">S18</option>
-                <option value="S19">S19</option>
-                <option value="S20">S20</option>
-              </select>
-            </div>{" "}
-            <div className="mt-4 w-1/2">
-              <label htmlFor="Branch" className="block mb-1 text-sm">
-                Branch
-              </label>
-              <select
-                id="Branch"
-                className="md:h-10 block w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-              >
-                <option>Select Branch</option>
-                <option value="AIML">AIML</option>
-                <option value="CE">CE</option>
-                <option value="CSE">CSE</option>
-                <option value="CSE-AIML">CSE-AIML</option>
-                <option value="CSE-DS">CSE-DS</option>
-                <option value="CSE-Hindi">CSE-Hindi</option>
-                <option value="CS">CS</option>
-                <option value="CSIT">CSIT</option>
-                <option value="ECE">ECE</option>
-                <option value="EN">EN</option>
-                <option value="IT">IT</option>
-                <option value="ME">ME</option>
-              </select>
-            </div>
-          </div>
-
-          <div className="flex space-x-4 w-full">
-            <div className="mt-4 w-1/2">
-              <label htmlFor="Gender" className="block mb-1 text-sm">
-                Gender
-              </label>
-              <select
-                id="Gender"
-                className="md:h-10 block w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-              >
-                <option>Select Gender </option>
-                <option value="Male"> Male</option>
-
-                <option value="Female"> Female</option>
-                <option value="Other">Other</option>
-              </select>
-            </div>{" "}
-            <div className="mt-4 w-1/2">
-              <label htmlFor="Hosteller" className="block mb-1 text-sm">
-                Hosteller
-              </label>
-              <select
-                id="Hostellers"
-                className="md:h-10 w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-              >
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
-              </select>
-            </div>
-          </div>
-
-          <button
-            type="submit"
-            className="bg-[#de17ce] hover:bg-[#c413b5] text-white font-normal py-2 px-4 w-full mt-6 rounded-md"
+    <TracingBeam className="px-6">
+      <section className="min-h-screen flex">
+        <div className="bg-black w-1/3 h-full"></div>
+        <div className="bg-white md:px-8 py-4 mx-auto leading-10 w-3/4">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="font-work-sans font-semibold text-xl w-[80%] mx-auto"
           >
-            Register
-          </button>
-        </form>
-      </div>
-    </section>
+            <h1 className="vision-head text-center mt-4 font-bungeeInline">
+              VISION
+            </h1>
+            <div className="mt-8">
+              <label htmlFor="name" className="block mb-1 text-sm col-12">
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Name"
+                {...register("name", {
+                  required: {
+                    value: true,
+                    message: "Name is required",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Name should be atleast 3 characters",
+                  },
+                })}
+                className="md:h-10 w-full rounded-md border text-black  border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+              />
+              {errors.name && (
+                <span className="text-red-500 text-xs">
+                  {errors.name.message}
+                </span>
+              )}
+            </div>
+
+            <div className="mt-4">
+              <label htmlFor="Email" className="block mb-1 text-sm">
+                College Email
+              </label>
+              <input
+                type="text"
+                id="email"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email is required",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@akgec\.ac\.in/,
+                    message: "Please enter a valid college email",
+                  },
+                })}
+                placeholder="@akgec.ac.in"
+                className="md:h-10 w-full rounded-md border text-black  border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+              />
+              {errors.email && (
+                <span className="text-red-500 text-xs">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+
+            <div className="mt-4">
+              <label htmlFor="PhoneNumber" className="block mb-1 text-sm">
+                Phone Number
+              </label>
+              <input
+                type="tel"
+                id="PhoneNumber"
+                placeholder="Phone Number"
+                {...register("phoneNumber", {
+                  required: {
+                    value: true,
+                    message: "Phone number is required",
+                  },
+                  pattern: {
+                    value: /^(?:\+91)?\d{10}$/,
+                    message: "Please enter a valid phone number",
+                  },
+                })}
+                className="md:h-10 w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+              />
+              {errors.phoneNumber && (
+                <span className="text-red-500 text-xs">
+                  {errors.phoneNumber.message}
+                </span>
+              )}
+            </div>
+
+            <div className="mt-4">
+              <label htmlFor="studentNumber" className="block mb-1 text-sm">
+                Student Number
+              </label>
+              <input
+                type="text"
+                id="studentNumber"
+                {...register("studentId", {
+                  required: {
+                    value: true,
+                    message: "Student number is required",
+                  },
+                  minLength: {
+                    value: 5,
+                    message: "Enter a valid student number",
+                  },
+                })}
+                placeholder="student Number"
+                className="h-10 w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+              />
+              {errors.studentId && (
+                <span className="text-red-500 text-xs">
+                  {errors.studentId.message}
+                </span>
+              )}
+            </div>
+
+            <div className="flex space-x-4 w-full">
+              <div className="mt-4 w-1/2">
+                <label htmlFor="Section" className="block mb-1 text-sm">
+                  Section
+                </label>
+                <select
+                  id="Section"
+                  {...register("section", { required: true })}
+                  className="md:h-10 block w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                >
+                  {sections.map((section, index) => (
+                  <option key={index} value={section}>{section}</option>
+                ))}
+                </select>
+              </div>{" "}
+              <div className="mt-4 w-1/2">
+                <label htmlFor="Branch" className="block mb-1 text-sm">
+                  Branch
+                </label>
+                <select
+                  id="Branch"
+                  {...register("branch", { required: true })}
+                  className="md:h-10 block w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                >
+                  {branches.map((branch, index) => (
+                  <option key={index} value={branch}>{branch}</option>
+                ))}
+                </select>
+              </div>
+            </div>
+
+            <div className="flex space-x-4 w-full">
+              <div className="mt-4 w-1/2">
+                <label htmlFor="Gender" className="block mb-1 text-sm">
+                  Gender
+                </label>
+                <select
+                  id="Gender"
+                  {...register("gender")}
+                  className="md:h-10 block w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                >
+                  <option value="Male"> Male</option>
+                  <option value="Female"> Female</option>
+                  <option value="Other">Other</option>
+                </select>
+                
+              </div>
+              <div className="mt-4 w-1/2">
+                <label htmlFor="Hosteller" className="block mb-1 text-sm">
+                  Hosteler
+                </label>
+                <select
+                  id="Hostellers"
+                  className="md:h-10 w-full rounded-md border text-black border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 placeholder:text-base focus:outline-none focus:ring-1 focus:ring-gray-200 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                >
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                </select>
+              </div>
+            </div>
+            
+            <button
+              type="submit"
+              className="bg-[#de17ce] hover:bg-[#c413b5] text-white font-normal py-2 px-4 w-full mt-6 rounded-md"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </section>
+    </TracingBeam>
   );
 }
--- src/type.d.ts
+++ src/type.d.ts
@@ -5,3 +5,49 @@ export interface WorkCard {
     splineUrl: string;
     thumb: string;
 }
+
+
+export type FormValues = {
+    name: string;
+    email: string;
+    studentId: string;
+    phoneNumber: string;
+    gender: "Male" | "Female" | "Other";
+    branch:
+      | "AIML"
+      | "CE"
+      | "CSE"
+      | "CSE-AIML"
+      | "CSE-DS"
+      | "CSE-Hindi"
+      | "CS"
+      | "CSIT"
+      | "ECE"
+      | "EN"
+      | "IT"
+      | "ME";
+  
+    residency: "Hosteller" | "Day scholar";
+    section:
+      | "S1"
+      | "S2"
+      | "S3"
+      | "S4"
+      | "S5"
+      | "S6"
+      | "S7"
+      | "S8"
+      | "S9"
+      | "S10"
+      | "S11"
+      | "S12"
+      | "S13"
+      | "S14"
+      | "S15"
+      | "S16"
+      | "S17"
+      | "S18"
+      | "S19"
+      | "S20";
+  };
+  
